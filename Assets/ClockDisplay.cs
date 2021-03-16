26b887ebfc8111c912d7e3a893b3e88b2f8eeca5
Jun Shimura <junshimura@gmail.com>
2021-03-16 17:59:17 +0900

    1st

---
+++ Assets/ClockDisplay.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockDisplay : MonoBehaviour
+{
+    [SerializeField]
+    Text clockLabel;
+    [SerializeField]
+    Transform arrow1;
+    [SerializeField]
+    Transform arrow2;
+    [SerializeField]
+    Transform arrow3;
+
+    System.DateTime dateTime = new System.DateTime();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dateTime = System.DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dateTime = System.DateTime.Now;
+        clockLabel.text
+            = dateTime.Hour.ToString() + ":"
+            + dateTime.Minute.ToString() + ":"
+            + dateTime.Second.ToString();
+    }
+}
