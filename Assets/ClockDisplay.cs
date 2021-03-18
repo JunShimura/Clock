36f118a4eae17d7d76e675f017eaa90bd1440886
Jun Shimura <junshimura@gmail.com>
2021-03-18 17:02:19 +0900

    Rotate Arrow

--- Assets/ClockDisplay.cs
+++ Assets/ClockDisplay.cs
@@ -14,6 +14,8 @@ public class ClockDisplay : MonoBehaviour
     Transform arrow2;
     [SerializeField]
     Transform arrow3;
+    [SerializeField]
+    Transform arrow4;
 
     System.DateTime dateTime = new System.DateTime();
 
@@ -27,6 +29,24 @@ public class ClockDisplay : MonoBehaviour
     void Update()
     {
         dateTime = System.DateTime.Now;
+        float hour = dateTime.Hour;
+        float minute = dateTime.Minute;
+        float second = dateTime.Second;
+        float ms = dateTime.Millisecond;
+        clockLabel.text
+            = hour.ToString("00") + ":"
+            + minute.ToString("00") + ":"
+            + second.ToString("00")+":"
+        +ms.ToString("000");
+        arrow1.rotation = Quaternion.Euler(0, 0, (hour+(minute+second/60)/60) * -(360 / 12));
+        arrow2.rotation = Quaternion.Euler(0, 0, minute * -(360 / 60));
+        arrow3.rotation = Quaternion.Euler(0, 0, second * -(360 / 60));
+        arrow4.rotation = Quaternion.Euler(0, 0, -ms* (360.0f /1000.0f));
+    }
+
+/*    void Update()
+    {
+        dateTime = System.DateTime.Now;
         int hour = dateTime.Hour;
         int minute = dateTime.Minute;
         int second = dateTime.Second;
@@ -37,5 +57,5 @@ public class ClockDisplay : MonoBehaviour
         arrow1.rotation = Quaternion.Euler(0, 0, dateTime.Hour*-(360/12));
         arrow2.rotation = Quaternion.Euler(0, 0, dateTime.Minute * -(360 / 60));
         arrow3.rotation = Quaternion.Euler(0, 0, dateTime.Second * -(360 / 60));
-    }
+    }*/
 }
