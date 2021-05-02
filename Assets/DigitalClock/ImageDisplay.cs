67a32050f4e9b214e78a16ccc17a4ec23725194f
Jun Shimura <junshimura@gmail.com>
2021-05-02 17:01:22 +0900

    ImageDisplay on DigitalClock

---
+++ Assets/DigitalClock/ImageDisplay.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using CommonTools;
+using DigitalClock;
+
+
+namespace DigitalClock
+{
+    [RequireComponent(typeof(Image))]
+    public class ImageDisplay : ValueDisplay
+    {
+        Image image;
+
+        private void Awake()
+        {
+            image = GetComponent<Image>();
+            if (image == null) {
+                Debug.LogError("Image is not found st Start");
+            }
+        }
+        public override float SetValue(float val)
+        {
+            if (image == null) {
+                Debug.LogError("Image is not found at SetValue");
+                return val;
+            }
+            image.fillAmount = val;
+            return val;
+        }
+    }
+}
